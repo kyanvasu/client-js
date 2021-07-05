08ccfa8005f988d3ae137671bc1162dade728a82
Rogelio Morey <sparohawk@gmail.com>
2021-07-05 09:46:49 -0400

    feat: auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number): number => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import Base from "./modules/base";
+
+const base = new Base();
+
+export default base;
