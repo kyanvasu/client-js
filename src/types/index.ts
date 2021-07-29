3c6e5f9025303e0ab91c7ca793048fd610658db6
Rogelio Morey <sparohawk@gmail.com>
2021-07-29 13:32:28 -0400

    feat: add index with types export

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export { default as ApplicationSettings } from './application-settings';
+export { default as ClientOptions } from './client-options';
+export { File, FileAttribute } from './file.interface';
+export { FormatedResponse } from './formated-response.interface';
+export { default as HttpError } from './http-error.type';
+export { Location as Location } from './location.interface';
+export { default as LoginResponse } from './login-response.interface';
+export { PaginationArgument } from './pagination-argument';
+export { default as KanvasStorage } from './storage.interface';
+export { UserInterface, CreateUserParams, CreatedUser } from './user.interface';
