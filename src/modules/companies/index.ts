10b2e7ee5c29af6a2bf3c59cfc91e0e5ff497132
Rogelio Morey <sparohawk@gmail.com>
2021-08-05 18:46:08 -0400

    fix: interface imports, module extends generics

--- src/modules/companies/index.ts
+++ src/modules/companies/index.ts
@@ -1,16 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/camelcase */
 /**
  * Kanvas SDK Companies Module
  */
 import Module from 'modules/module';
 import HttpClient from 'core/http-client';
-import { CompaniesInterface } from 'types/companies.interface';
+import { CompanyInterface, CreateCompanyParams } from 'types/companies.interface';
 
 /**
  * @description Kanvas Companies Module
  */
-export default class Companies extends Module<CompaniesInterface> {
+export default class Companies extends Module<CompanyInterface, CreateCompanyParams> {
   constructor(http: HttpClient) {
     super(http, '/companies');
   }
