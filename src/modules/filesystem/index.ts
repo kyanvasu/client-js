8cd031fefaf4d108e99ac0078f83a85f00bdb51b
Samuel J. Mancebo <sjmancebo@gmail.com>
2021-10-04 16:29:15 -0400

    feat: Add file upload for mobile devices using react native

--- src/modules/filesystem/index.ts
+++ src/modules/filesystem/index.ts
@@ -1,6 +1,6 @@
 import HttpClient from 'core/http-client';
 import Base from 'modules/base';
-import { File } from 'types/file.interface';
+import { File, RNMobileFileSignature } from 'types/file.interface';
 
 export default class FileSystem extends Base {
   public files: string[] = [];
@@ -9,11 +9,18 @@ export default class FileSystem extends Base {
     super(httpClient, '/filesystem');
   }
 
-  public async upload(file: Blob): Promise<File> {
+  public async upload(file: Blob, fieldName?: string): Promise<File[]> 
+  public async upload(file: Blob[], fieldName?: string): Promise<File[]> 
+  public async upload(files: Blob | Blob[], fieldName: string = 'files'): Promise<File[]> {
     const formData = new FormData();
-    formData.append('files', file);
-
-    const { data } = await this.http.request<File>({
+    if (Array.isArray(files)) {
+      files.forEach((item, index)  => {
+        formData.append(`${fieldName}-${index}`, item);
+      })
+    } else {
+      formData.append(fieldName, files);
+    }
+    const { data } = await this.http.request<File[]>({
       method: 'POST',
       url: this.baseUrl,
       headers: {
@@ -23,5 +30,22 @@ export default class FileSystem extends Base {
     });
 
     return data;
+  }
+
+  /**
+   * @description - File upload used on mobile devices (React Native)
+   * @param file {RNMobileFileSignature} | RNMobileFileSignature[] - React Native file signature
+   * @returns Promise<File[]>
+   */
+  public async uploadFromMobile(
+    f: RNMobileFileSignature | RNMobileFileSignature[],
+    fieldName?: string
+  ): Promise<File[]> {
+    if (Array.isArray(f)) {
+      return this.upload(f as unknown as Blob[], fieldName);
+    } else {
+      return this.upload(f as unknown as Blob, fieldName);
+    }
+    
   }
 }
