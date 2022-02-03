290bc1e9551662d5bdef50fc2fbcfbf651abf63f
Bryan Astacio <brya0196@gmail.com>
2022-02-03 14:42:33 -0400

    style: change double quotes for single quotes

--- src/modules/roles/index.tsx
+++ src/modules/roles/index.tsx
@@ -1,6 +1,6 @@
-import HttpClient from "core/http-client";
-import Module from "modules/module";
-import { RoleInterface } from "types/role.interface";
+import HttpClient from 'core/http-client';
+import Module from 'modules/module';
+import { RoleInterface } from 'types/role.interface';
 
 export default class Roles extends Module<RoleInterface> {
   constructor(http: HttpClient) {
