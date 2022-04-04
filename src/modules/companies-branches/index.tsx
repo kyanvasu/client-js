6aba8945a4df577c7d8977bd2f8fbbade59589a9
Bryan Astacio <brya0196@gmail.com>
2022-04-04 16:19:05 -0400

    feat: create method removeUser for companies and branches

--- src/modules/companies-branches/index.tsx
+++ src/modules/companies-branches/index.tsx
@@ -10,10 +10,36 @@ export default class CompaniesBranches extends Module<CompaniesBranchesInterface
     super(http, '/companies-branches');
   }
 
+  /**
+   * @description get all the users that belongs to the given company branch
+   * @param {number | string} id get the company branch users by id 
+   * @returns {UserInterface[]}
+   */
   async getUsers(id: number | string): Promise<UserInterface[]>;
+  /**
+   * @description get all the users that belongs to the given company branch
+   * @param {number | string} id get the company branch users by id 
+   * @param {PaginationArgument} pagination arguments filter the user data
+   * @returns {UserInterface[]}
+   */
   async getUsers(id: number | string, pagination: PaginationArgument): Promise<FormatedResponse<UserInterface>>;
   async getUsers(id: number | string, pagination: PaginationArgument = DEFAULT_PAGINATION_ARGUMENT): Promise<UserInterface[] | FormatedResponse<UserInterface>> {
     const url = `${this.baseUrl}/${id}/users`;
     return this.gettable<UserInterface>(url, pagination);
+  }
+
+  /**
+   * @description remove the given user by it id from the company branch
+   * @param {number | string} id company branch id 
+   * @param {number | string} userId user id to be removed
+   * @returns {Promise<string>}
+   */
+  async removeUser(id: number | string, userId: number | string): Promise<string> {
+    const { data } = await this.http.request<string>({
+      url: `${this.baseUrl}/${id}/users/${userId}`,
+      method: 'DELETE',
+    })
+
+    return data;
   }
 }
